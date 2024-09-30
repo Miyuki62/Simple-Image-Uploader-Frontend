a9ab8dc67cbd3d686a3096275f987f35be34578f
Steven Richart <59703755+Miyuki62@users.noreply.github.com>
2024-09-30 16:58:19 +0200

    loader show now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,16 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { LoaderComponent } from './loader/loader.component';
 import { DownloadComponent } from './download/download.component';
 import { UploadComponent } from './upload/upload.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, LoaderComponent, DownloadComponent, UploadComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    DownloadComponent,
+    UploadComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideClientHydration(), provideHttpClient(withFetch())],
   bootstrap: [AppComponent],
